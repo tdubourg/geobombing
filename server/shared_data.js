7b41cff280262a7fc1f7fb1ba66a4e7ce1d73ce0
tdubourg <tdubourg@users.noreply.github.com>
2014-02-05 15:08:52 +0100

    Basic server

---
+++ server/shared_data.js
@@ -0,0 +1,9 @@
+"use strict"
+
+var pgsql = null // TODO require('./pgsql')
+
+var TABLE_NAMES = { // shortens the code, and avoids spelldraws, in short, THIS IS [SPARTA?] CONSTANTS!
+}
+
+exports.TABLE_NAMES = TABLE_NAMES
+exports.pgsql = pgsql
