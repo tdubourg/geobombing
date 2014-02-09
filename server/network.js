ff8316de66dfdfd43217d306ae2d993e78b64b08
Julie <julie.seng@insa-lyon.fr>
2014-02-09 15:11:48 +0100

    ...

---
+++ server/network.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Utils for network
+var FRAME_SEPARATOR = "\n"
+
+var decode_frame = function (frame_string) {
+	return {type: "dummy", data: "frame_string"} //TODO implement that
+}
+
+exports.FRAME_SEPARATOR = FRAME_SEPARATOR
+exports.decode_frame = decode_frame
