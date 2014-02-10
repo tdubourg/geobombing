2f8b7ac9d80e62fe3cde54aefc0046bd29809d8c
arckain <arckain@hotmail.fr>
2014-02-10 11:01:03 +0100

    Adding file common.js useful functions to fill Map structure

--- server/Classes/clWay.js
+++ server/Classes/clWay.js
@@ -2,7 +2,7 @@
 function clWay() 
 {
     //Attributs    
-    this.wayName = "";
     this.wayId = 0;
-    this.wayListNode = null; // Array of Node contained in the way
+	this.wayName = "";
+    this.wayListNodeId = null; // Array of NodeId contained in the way
 };
