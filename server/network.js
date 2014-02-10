3a1908eb0cbce0b4b7c714c53fc901118c140477
arckain <arckain@hotmail.fr>
2014-02-10 09:30:28 +0100

    Added classes of model of map data structure git status

--- server/network.js
+++ server/network.js
@@ -7,5 +7,6 @@ var decode_frame = function (frame_string) {
 	return {type: "dummy", data: "frame_string"} //TODO implement that
 }
 
+// provides "attributes" and "methods" for the "class" shared_data.js
 exports.FRAME_SEPARATOR = FRAME_SEPARATOR
 exports.decode_frame = decode_frame
