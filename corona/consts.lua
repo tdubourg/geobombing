d0d103d553497028cbf3921cdb8b506f3d2ae651
ychazallon <windows@wynock>
2014-02-10 16:31:57 +0100

    loading network nodes. No ways for now

---
+++ corona/consts.lua
@@ -0,0 +1,7 @@
+-- JSON names
+JSON_MAP_NAME = "mapName"
+JSON_NODE_LIST = "mapListNode"
+JSON_WAY_LIST = "mapListWay"
+JSON_NODE_UID = "nodeId"
+JSON_NODE_LAT = "nodeLatitude"
+JSON_NODE_LON = "nodeLongitude"
