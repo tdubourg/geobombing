7bee139fb1ea24e9dd7147d6643827def0871699
arckain <arckain@hotmail.fr>
2014-02-12 11:36:15 +0100

    get gpt and send it to bd

--- server/network.js
+++ server/network.js
@@ -5,18 +5,10 @@ var FRAME_SEPARATOR = "\n"
 
 var decode_frame = function (frame_string) 
 {
-	var decoded_frame = JSON.parse(frame_string); // deserialization JSON
-	var frame;
-	switch (decoded_frame.type)
-    { 
-        case "move": frame = {type: "move", data: decoded_frame.data}; break;
-        case "bomb": frame = {type: "bomb", data: decoded_frame.data}; break;
-        case "map": frame = {type: "map", data: decoded_frame.data}; break;
-        case "gps": frame = {type: "gps", data: decoded_frame.data}; break;
-        default: frame = {type: "unknown", data: decoded_frame.data}; break;
-    }
-	//var frame = {type: "map", data: "frame_string"}; //TODO implement that
-	return frame;
+	var decoded_frame;
+	try{decoded_frame = JSON.parse(frame_string);} // deserialization JSON
+	catch (e){decoded_frame = {type: "unknown", data: frame_string};}
+	return decoded_frame;
 }
 
 // provides "attributes" and "methods" for the "class" server.js
