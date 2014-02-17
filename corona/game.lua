58bce73c74011760af9ede23414e423d77117bfd
Julie <julie.seng@insa-lyon.fr>
2014-02-17 11:00:37 +0100

    correction

--- corona/game.lua
+++ corona/game.lua
@@ -5,62 +5,102 @@
 ----------------------------------------------------------------------------------
 
 local storyboard = require( "storyboard" )
+print("1")
 local Player = require( "player" )
+print("2")
 local network = require ("network")
+print("3")
 local scene = storyboard.newScene()
+print("4")
 require "node"
+print("5")
 require "consts"
+print("6")
 local camera = require "camera"
+print("7")
 require "vector2D"
+print("8")
 require "map"
+print("9")
 require "items"
+print("10")
 local physics = require( "physics" )
-
+print("11")
 local playBtn
+print("12")
 player = nil -- global in order to be accessed from everywhere
-
+print("13")
 local bombBtn = nil
+print("14")
 local currentMap = nil
+print("15")
 itemsManager = nil
+print("16")
 local gui = require ("gui") -- has to be required after globals definition
+print("17")
+
 
 -- Called when the scene's view does not exist:
 function scene:createScene( event )
+	print("18")
 	local group = self.view
+	print("19")
 	displayMainGroup:insert(group)
+	print("20")
 	camera.initCamera()
+	print("21")
 	gui.initGUI()
+	print("22")
 
 	delay=1
+	print("23")
 	physics.start( )
+	print("24")
 
 	-- connect to server
 	network.connect_to_server("192.168.43.119", 3000)
+	print("25")
 	network.sendPosition()
+	print("26")
 	luaMap = network.receiveSerialized()	-- for now, first frame received is map. TODO: add listeners
-
+	print("27")
 	currentMap = Map:new(luaMap)
-
+	print("28")
 	player = Player.new( "Me",  2)
+	print("29")
 	itemsManager = ItemsManager.new()
+	print("29")
 	print ("IM", itemsManager)
+	print("29")
 end
-
+	print("29")
 local myListener = function( event )
+	print("29")
 	if (btnBombClicked) then
+	print("29")
 		btnBombClicked = false
+	print("29")
 	else
+	print("29")
 		player:refresh()
+	print("29")
 		lookAt(player.pos)
+	print("29")
 	end
+	print("29")
 end
-
 local trans
+	print("29")
 local function moveObject(e)
+	print("29")
 		print ("Received tap event in moveObject")
+	print("29")
 		if(trans)then
+	print("29")
 			transition.cancel(trans)
+	print("29")
 		end
+	print("29")
 
 		--find nearest node
 
@@ -72,32 +112,50 @@ local function moveObject(e)
 	 
 		--player:saveNewNodes(currentMap.nodesByUID)
 
+	print("29")
 		if (btnBombClicked) then
+	print("29")
 			btnBombClicked = false
+	print("29")
 		else
+	print("29")
 			local screenPos = Vector2D:new(e.x,e.y)
 			local worldPos = screenToWorld(screenPos)
+	print("29")
 			local node = currentMap:getClosestNode(worldPos)
+	print("29")
 			-- print ("Closest node is", node)
 			--print(n6.pos.x .." ,".. n6.pos.y .." ")
 			local from = currentMap:getClosestNode(player.pos)
+	print("29")
 			-- print ("Closest from is", from)
 			
 			if (from == node ) then
+	print("29")
 				print("la1")
+	print("29")
 				--player:saveNewDestination(e)
 			elseif (from == nil) then
+	print("29")
 				print("la2")
+	print("29")
 				--player:saveNewDestinationVect(node.pos)
 			elseif (node == nil) then
+	print("29")
 				print("la3")
+	print("29")
 				--player:saveNewDestination(e)
 			-- print ("Nodes:", nodes)
 			else
+	print("29")
 			local nodes = currentMap:findPath(from, node)
+	print("29")
 			player:saveNewNodes(nodes)
+	print("29")
 			end
+	print("29")
 		end
+	print("29")
 
 
 	end
