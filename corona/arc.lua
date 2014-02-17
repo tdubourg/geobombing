9b21f4d3f62027d0dfd0aa5c1adfebb2e5de9db2
ychazallon <windows@wynock>
2014-02-17 10:35:41 +0100

    untested fix for pathfinding on server map. (uid as string)

--- corona/arc.lua
+++ corona/arc.lua
@@ -5,7 +5,7 @@ function Arc:new(end1, end2)  -- The constructor
   local object = {end1=end1, end2=end2}
   object.len = end1.pos:dist(end2.pos)
 
-  --TODO : display group
+  --TODO : display groupS
 
   object.drawable = display.newLine(end1.pos.x, end1.pos.y, end2.pos.x, end2.pos.y )
   object.drawable.strokeWidth = 4
