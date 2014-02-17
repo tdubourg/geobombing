1e0a5f6701d0ccebbfdfbb77a863e7f659e8ef06
arckain <arckain@hotmail.fr>
2014-02-17 17:13:04 +0100

    fonction de calcul de position en envoi position au client par le serveur

--- server/frame_action.js
+++ server/frame_action.js
@@ -36,35 +36,59 @@ var sendmap_action = function (frame_data, stream)
 	}); // lat, lon
 }
 
-var test_action = function (frame_data, stream) 
-{
-	console.log("test_action:\n" + frame_data);
-}
-
 
 var move_action = function (frame_data, stream) 
 {
 	console.log("move_action:\n" + frame_data);
 	nb_instance_move++; // to stop previous moving
-	var node;
-	if (frame_data != null && frame_data.start_edge_pos != null 
-		&& frame_data.end_edge_pos != null) {node = utils.CreatePosition(1, 2, frame_data.end_edge_pos);}
-	setTimeout(function(){multiple_send_position(stream)}, 1000); // execute the function every 1000ms
-}
-var multiple_send_position = function (stream) 
-{
-        var pos = utils.CreatePosition(0, 0, 1)
-		var content = 
-		{
-			"type": TYPEPOS, 
-			"data": pos
-		};
 
-		var data = JSON.stringify(content); // parsage JSON
-		stream.write(data + FRAME_SEPARATOR, function () {console.log("PosData sent:\n" + data)})
-		if (nb_instance_move < 2) {setTimeout(function(){multiple_send_position(stream)}, 1000);}
-		else nb_instance_move--;
+	//decode frame
+	var pos;
+	if (frame_data != null && frame_data.start_edge_pos != null && frame_data.end_edge_pos != null
+		&& frame_data.nodes != null && frame_data.nodes.length > 0) 
+		{
+			var startedge = parseFloat(frame_data.start_edge_pos);
+			var endedge = parseFloat(frame_data.end_edge_pos);
+
+			// send answer
+			setTimeout(function(){multiple_send_position(stream, startedge, endedge, frame_data.nodes)}, 1000); // execute the function every 1000ms
+		}
+}
+var multiple_send_position = function (stream, startedge, endedge, idnodes) 
+{
+    // calculate position
+    if (idnodes.length == 2 && startedge >= endedge) startedge = endedge; // add stop send
+	else if (startedge < 1) startedge += 0.2;
+	else if (startedge >= 1)
+	{
+		startedge = 0;
+		if (idnodes.length > 2) idnodes.shift();
+	}
+
+    var position = utils.CreatePosition(idnodes[0], idnodes[1], startedge);
+	var content = 
+	{
+		"type": TYPEPOS, 
+		"data": position
+	};
+	var data = JSON.stringify(content); // parsage JSON
+	stream.write(data + FRAME_SEPARATOR, function () {console.log("PosData sent:\n" + data)})
+	if (nb_instance_move < 2) {setTimeout(function(){multiple_send_position(stream, startedge, endedge, idnodes)}, 1000);}
+	else nb_instance_move--;
         
+}
+var calculate_position= function (startedge, endedge, idnodes)
+{
+	if (idnodes.length == 2 && startedge >= endedge) startedge = endedge;
+	else if (startedge < 1) startedge += 0.2;
+	else if (startedge >= 1)
+	{
+		startedge = 0;
+		if (idnodes.length > 2) idnodes.shift();
+	}
+
+    var pos = utils.CreatePosition(idnodes[0], idnodes[1], startedge);
+	return pos;
 }
 
 var bomb_action = function (frame_data, stream) 
@@ -86,7 +110,6 @@ var frame_actions =
 	//Type from client
 	"gps":  sendmap_action, // reponse function to localise
 	"move": move_action,
-	"bomb": bomb_action,
-	"test": test_action
+	"bomb": bomb_action
 }
 exports.frame_actions = frame_actions
