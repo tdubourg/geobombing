ff977c7eebfdce59156d03cf916fcc1ec91bed32
Julie <julie.seng@insa-lyon.fr>
2014-02-18 14:35:38 +0100

    modif

--- corona/game.lua
+++ corona/game.lua
@@ -107,13 +107,11 @@ local function moveObject(e)
 			else
 			local nodes = currentMap:findPath(from, node)
 
-
 			local toPos = currentMap:getClosestPos(worldPos)
-print("Coucou")
 			net.sendPathToServer(nodes)
 			player.nodeFrom=from
 			--player.nodeTo=nodes[1]
-			print(toPos[1].end1.uid .."  youhou ".. toPos[2])
+			print(toPos[1].end1.uid .."  ratio ".. toPos[2])
 			--player:goToAR(toPos[1],toPos[2])
 
 			player:saveNewNodes(nodes)
