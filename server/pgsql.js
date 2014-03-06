14992933eba479a9ec1c386ac3689c5250697908
arckain <arckain@hotmail.fr>
2014-03-06 17:37:45 +0100

    pour pouvoir puller

--- server/pgsql.js
+++ server/pgsql.js
@@ -1,11 +1,11 @@
 
 "use strict"
-var clMap = require("./Classes/clMap").clMap; 
-var common = require("./common");
-var conDB = false;
-var qh = conDB? require('./query_helper'): null; // for generic query
-var lastMapId = 1;
-var lastNodeId = 1;
+var clMap = require("./Classes/clMap").clMap
+var common = require("./common")
+var conDB = false
+var qh = conDB? require('./query_helper'): null // for generic query
+var lastMapId = 1
+var lastNodeId = 1
 var u = require("./util")
 //var t = require("./tiles")
 
