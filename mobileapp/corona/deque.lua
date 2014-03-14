0bd2c2f25ed68406777afae7e4174189a6cb5fd5
tdubourg <tdubourg@users.noreply.github.com>
2014-03-14 16:04:44 +0100

    OOps, wrong copy/paste

--- mobileapp/corona/deque.lua
+++ mobileapp/corona/deque.lua
@@ -10,14 +10,14 @@ function Deque.pushleft (deque, value)
 	local first = deque.first - 1
 	deque.first = first
 	deque[first] = value
-	a.length = a.length + 1
+	deque.length = deque.length + 1
 end
 
 function Deque.pushright (deque, value)
 	local last = deque.last + 1
 	deque.last = last
 	deque[last] = value
-	a.length = a.length + 1
+	deque.length = deque.length + 1
 end
 
 function Deque.popleft (deque)
@@ -26,19 +26,19 @@ function Deque.popleft (deque)
 	local value = deque[first]
 	deque[first] = nil        -- to allow garbage collection
 	deque.first = first + 1
-	a.length = a.length - 1
+	deque.length = deque.length - 1
 	return value
 end
 
 function Deque.first(deque)
-	if (a.length == 0) then
+	if (deque.length == 0) then
 		return nil
 	end
 	return deque[deque.first]
 end
 
 function Deque.last(deque)
-	if (a.length == 0) then
+	if (deque.length == 0) then
 		return nil
 	end
 	return deque[deque.first]
@@ -50,7 +50,7 @@ function Deque.popright (deque)
 	local value = deque[last]
 	deque[last] = nil         -- to allow garbage collection
 	deque.last = last - 1
-	a.length = a.length - 1
+	deque.length = deque.length - 1
 	return value
 end
 
